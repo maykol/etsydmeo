15b49fd003f1d30927669aa682623fdbc8474be9
maykol <maykolpf@hotmail.com>
2014-05-14 15:49:08 -0500

    Install and setup Devise gem

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,4 +28,7 @@ Etsydemo::Application.configure do
   config.assets.debug = true
 
   Paperclip.options[:command_path] = 'C:\Program Files\GnuWin32\bin'
+
+  #Require for Device gem
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,7 @@ Etsydemo::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  #Use Devise gem 
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
