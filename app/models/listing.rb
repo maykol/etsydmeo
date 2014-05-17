76e986e4a52ec3612cc101ab0f034574e43bc075
maykol <maykolpf@hotmail.com>
2014-05-17 16:49:55 -0500

    Enable Users to place Orders

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -14,5 +14,7 @@ class Listing < ActiveRecord::Base
     validates :name, :description, :price, presence: true
   	validates :price, numericality: { greater_than: 0 }
 
+
   	belongs_to :user
+  	has_many :orders
 end
